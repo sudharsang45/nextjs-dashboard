f7bde8d2276584dba79955fa31c7a086c0c39897
Sudharsan Ganesan <sudharsang45@protonmail.com>
2025-05-08 14:45:52 -0400

    fixed autherror type does not exist error

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -124,11 +124,10 @@ export async function authenticate(
     await signIn('credentials', formData);
   } catch (error) {
     if (error instanceof AuthError) {
-      switch (error.type) {
-        case 'CredentialsSignin':
-          return 'Invalid credentials.';
-        default:
-          return 'Something went wrong.';
+      if (error.message?.includes('CredentialsSignin')) {
+        return 'Invalid credentials.';
+      } else {
+        return 'Something went wrong.';
       }
     }
     throw error;
